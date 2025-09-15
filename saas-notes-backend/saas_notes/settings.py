080fcd53c11dd5ededed70ba59f8bd3be150747b
Anshika Rai <anshikacoder10@gmail.com>
2025-09-15 15:58:50 +0530

    Backend settings updated for deployment

--- saas-notes-backend/saas_notes/settings.py
+++ saas-notes-backend/saas_notes/settings.py
@@ -12,22 +12,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+import dj_database_url
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1kcjuv-x&pymi_s^har6s8__iv4h-zoasi8=q*+l-+qgy0g9(7"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+# Security
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = False   
+ALLOWED_HOSTS = ["*"]  
 
 # Application definition
 
@@ -55,9 +52,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # frontend dev URL
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # frontend dev URL
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -90,11 +88,17 @@ WSGI_APPLICATION = "saas_notes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
